90e43e9f55c10309a4c207b7e6e884e86821b533
Lenz Gschwendtner <lenz@springtimesoft.com>
2012-09-07 13:01:32 +0400

    first cleanup

--- lib/CatalystX/OAuth2/Schema/ResultSet/Client.pm
+++
@@ -1,9 +0,0 @@
-package CatalystX::OAuth2::Schema::ResultSet::Client;
-use parent 'DBIx::Class::ResultSet';
-
-sub find_refresh {
-  shift->related_resultset('codes')->search( { is_active => 1 } )
-    ->related_resultset('refresh_tokens')->find(@_);
-}
-
-1;
